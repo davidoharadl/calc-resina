f15dcfbd7830cf9e00364ae7c53898231b35abf5
David Ohara <davidoharadl@gmail.com>
2023-02-14 21:35:28 -0300

    validacao dos dados passados pelo user

--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,25 @@
+const btCalc = document.querySelector("#bt-calc");
+
+btCalc.addEventListener('click', calculateResina);
+
 function calculateResina() {
-    let typeResina = document.querySelector('#type_resina').value;
-    let weightPiece = document.querySelector('#weight-piece').value;
+    let typeResina = parseFloat(document.querySelector('#type_resina').value);
+    let weightPiece = parseInt(document.querySelector('#weight-piece').value);
+    // weightPiece = parseInt(weightPiece, 10);
+    console.log(typeof weightPiece);
+    console.log(typeof typeResina);
+    
     const fieldResina = document.querySelector('#amount-resina-piece');
     const fieldEndurecedor = document.querySelector('#amount-endurecedor-piece');
     let resina, endurecedor;
 
-    console.log(typeResina);
-
     resina = Math.floor((weightPiece / typeResina))
     endurecedor = weightPiece - resina
+
+    if (Number.isNaN(resina) || Number.isNaN(endurecedor)) {
+        alert("Por favor digite um numero inteiro!!");
+        return;
+    }
 
     fieldResina.value = resina;
     fieldEndurecedor.value = endurecedor;
